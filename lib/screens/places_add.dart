5c78eb32c4d24dc6ddb86e7ee9e6299d0b34c8a4
Mohit Lahoti <engtechno25@gmail.com>
2024-10-13 00:22:09 +0530

    Adding places logic inserted

---
+++ lib/screens/places_add.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class AddPlacesList extends StatefulWidget {
+  const AddPlacesList({super.key});
+
+  @override
+  State<AddPlacesList> createState() {
+    return _AddPlacesList();
+  }
+}
+
+class _AddPlacesList extends State<AddPlacesList> {
+  final _titleController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add New Place"),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(labelText: "Title"),
+              controller: _titleController,
+              style: TextStyle(color: Theme.of(context).colorScheme.onSurface),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.add),
+              label: const Text("Add Place"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
